332740e922dfcbe0c31c8f4ea459d8dbabc58862
fukugit <fukuma1234@gmail.com>
2021-01-06 22:53:18 +0900

    基本構文を追加

--- 1-basic/js/main.js
+++ 1-basic/js/main.js
@@ -1,13 +1,38 @@
 'use strict';
 
 /* 
-  双方向データバインディングです。 
+  もっとも基本的な構文です。
 */
-var vm = new Vue({
-  // id 名称を指定します。
-  el: '#app',
+let app1 = new Vue({
+  el: '#app1',
   data: {
-    message: 'vue!'
+    message: 'hello vue! from app1'
   }
 });
 
+/* 
+  メソッド定義します。  
+  テンプレート側からこのメソッドは呼び出せます。
+*/
+let app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'hello vue! from app2'
+  },
+  methods: {
+    sayHi: function() {
+      return this.message;
+    }
+  }
+});
+
+/* 
+  v-thmlを利用する用です。
+ */
+let app3 = new Vue({
+  el: '#app3',
+  data: {
+    html1: '<h1>hello vue! from app3<h2>',
+    html2: '<h2>hello vue! from app3<h2>'
+  }
+});
